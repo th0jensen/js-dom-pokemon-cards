2f4a2c6119cede40105321206e209deec4477ecf
Thomas Jensen <thomas@macbook-pro.tail75cdf.ts.net>
2024-05-31 00:55:34 +0200

    Completed core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
+document.body.onload = addPokemonCards()
 
-console.log(data);
+function addPokemonCards() {
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  // Grab the entry point on the HTML
+  const cardsUL = document.querySelector('.cards')
+
+  // Loop through all of the entries in data.js
+  for (let i = 0; i < data.length; i++) {
+
+    const pokemon = data[i]
+
+    // Create the list entry (card)
+    const card = document.createElement('li')
+    card.classList.add('card')
+
+    // Create the title for the card
+    const pokemonName = document.createElement('h2')
+    pokemonName.classList.add('card--title')
+    // Convert the first character to upper case
+    const pokemonNameCase = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    pokemonName.innerText = pokemonNameCase
+
+    // Create the image of the Pokemon card
+    const pokemonCover = document.createElement('img')
+    pokemonCover.classList.add('card--img')
+    pokemonCover.src = pokemon.sprites.other['official-artwork'].front_default
+
+    // Create the list where all the stats will be shown
+    const pokemonStats = document.createElement('ul')
+    pokemonStats.classList.add('card--text')
+
+    // Loop through the stats and add them to the list
+    for (let j = 0; j < pokemon.stats.length; j++) {
+      const stat = pokemon.stats[j]
+      const pokemonStatItem = document.createElement('li')
+      pokemonStatItem.innerText = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
+      pokemonStats.appendChild(pokemonStatItem)
+    }
+
+    // Append all the items to the card
+    card.appendChild(pokemonName)
+    card.appendChild(pokemonCover)
+    card.appendChild(pokemonStats)
+
+    // Append the card to the HTML page
+    cardsUL.appendChild(card)
+  }
+}
